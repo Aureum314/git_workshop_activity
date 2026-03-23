7f7544638dd874d78858a82348551bcba8c51736
Reece Harrison <reeceharrison9878@gmail.com>
2026-03-23 18:25:55 +1100

    Update CLOCK to be 50 Hz and fix polling loop

---
+++ workshop3nios/obj/default/hello_world.d
@@ -0,0 +1,12 @@
+obj/default/hello_world.o: hello_world.c \
+ ../workshop3nios_bsp//HAL/inc/io.h \
+ ../workshop3nios_bsp//HAL/inc/alt_types.h ../workshop3nios_bsp/system.h \
+ ../workshop3nios_bsp/linker.h
+
+../workshop3nios_bsp//HAL/inc/io.h:
+
+../workshop3nios_bsp//HAL/inc/alt_types.h:
+
+../workshop3nios_bsp/system.h:
+
+../workshop3nios_bsp/linker.h:
